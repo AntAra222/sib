69bf31a3948b87d11829f7007f18f796b5e270c8
AntAra222 <antcarmoaraujo@gmail.com>
2026-09-21 11:09:06 +0100

    Update __init__.py

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
-__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
+__author__ = "António Araujo" 
+__credits__ = ["António Araujo"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__maintainer__ = "Antonio Araujo"
+__email__ = "pg59752@uminho.pt"
 
 
 
